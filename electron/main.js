2bdc3432ae950db220341c0c486848c8902eef99
mims192 <mimansa192@gmail.com>
2026-04-08 15:26:44 +0530

    Changed Electron Wrapper

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,17 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
+const { spawn } = require('child_process');
 const path = require('path');
 
-let win;
+let win, tray, backendProcess;
+
+function startBackend() {
+  backendProcess = spawn('node', [path.join(__dirname, '../backend/server.js')], {
+    cwd: path.join(__dirname, '../backend'),
+    shell: true,
+  });
+  backendProcess.stdout.on('data', d => console.log(`Backend: ${d}`));
+  backendProcess.stderr.on('data', d => console.error(`Backend Error: ${d}`));
+}
 
 function createWindow() {
   win = new BrowserWindow({
@@ -9,16 +19,21 @@ function createWindow() {
     height: 650,
     webPreferences: { nodeIntegration: false },
   });
-
-  const isDev = !app.isPackaged;
-  win.loadURL(isDev
-    ? 'http://localhost:5173'
-    : `file://${path.join(process.resourcesPath, 'frontend/dist/index.html')}`
-  );
+const isDev = !app.isPackaged;
+win.loadURL(isDev
+  ? 'http://localhost:5173'
+  : `file://${path.join(process.resourcesPath, 'frontend/dist/index.html')}`
+);
 
   win.on('closed', () => (win = null));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  startBackend();
+  createWindow();
+});
 
-app.on('window-all-closed', () => app.quit());
+app.on('window-all-closed', () => {
+  if (backendProcess) backendProcess.kill();
+  app.quit();
+});
